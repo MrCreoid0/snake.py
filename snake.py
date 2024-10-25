a921de11256a995f9a7ce7e1d0a4109b760cc2da
MrCreoid <73683956+MrCreoid0@users.noreply.github.com>
2024-10-26 03:01:21 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -101,7 +101,9 @@ class SNAKE:
 
     def play_crunch_sound(self):
         self.crunch_sound.play()
+
     def reset(self):
+
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(0, 0)
 
@@ -149,6 +151,7 @@ class MAIN:
 
     def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or (not 0 <= self.snake.body[0].y < cell_number):
+            pygame.time.delay(2000)
             self.game_over()
 
         for block in self.snake.body[1:]:
@@ -175,18 +178,20 @@ class MAIN:
     def draw_score(self):
         score_text = str(len(self.snake.body) - 3)
         score_surface = game_font.render(score_text, True, (56, 74, 12))
-        score_x = int(cell_size * cell_number - 60)
-        score_y = int(cell_size * cell_number - 40)
+        score_x = int(cell_size * cell_number / 2 + 30)
+        score_y = int(cell_size)
         score_rect = score_surface.get_rect(center=(score_x, score_y))
         apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
-        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6, apple_rect.height)
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6,
+                              apple_rect.height)
 
         pygame.draw.rect(screen, (167, 209, 61), bg_rect)
         screen.blit(score_surface, score_rect)
         screen.blit(apple, apple_rect)
         pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
-pygame.mixer.pre_init(44100,-16,2,512)
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 
 cell_size = 40
@@ -198,8 +203,12 @@ game_font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
+pygame.display.set_caption("Snake")
 
 main_game = MAIN()
+
+pygame.mixer.music.load("Sound/bg_music.mp3")
+pygame.mixer.music.play(-1)
 
 while True:
     for event in pygame.event.get():
